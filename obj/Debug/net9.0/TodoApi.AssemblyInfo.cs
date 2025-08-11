d176bd00d6070655caa621a07ce511c5930ba5ce
Ven <venxxx.io@gmail.com>
2025-08-11 16:42:05 +0800

    Fix bug

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b99eec670d0b6a6821516d927be59c86b08640e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d91a6d705b933af4f04bff569afe7016c47bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
